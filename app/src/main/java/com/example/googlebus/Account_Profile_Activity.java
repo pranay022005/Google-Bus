ce257747ea2e66e3897e3811b23df214ab9dd218
pranay022005 <pranaygaykwad89@gmail.com>
2024-09-27 08:54:48 +0530

    account_profile_Activity Desing Frentent and backent Code

--- app/src/main/java/com/example/googlebus/Account_Profile_Activity.java
+++ app/src/main/java/com/example/googlebus/Account_Profile_Activity.java
@@ -10,6 +10,7 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -19,12 +20,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.googlebus.Comman.Urls;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class Account_Profile_Activity extends AppCompatActivity {
 
@@ -33,10 +44,7 @@ public class Account_Profile_Activity extends AppCompatActivity {
     AppCompatButton btnEditProfile, btnSignOut;
 
     SharedPreferences preferences;
-    String stringUsername;
-
-    GoogleSignInOptions googleSignInOptions;
-    GoogleSignInClient googleSignInClient;
+    String strUsername;
 
     ProgressDialog progressDialog;
 
@@ -49,7 +57,7 @@ public class Account_Profile_Activity extends AppCompatActivity {
 
         preferences = PreferenceManager.getDefaultSharedPreferences(Account_Profile_Activity.this);
 
-        stringUsername = preferences.getString("username", "");
+        strUsername = preferences.getString("username", "");
 
         ivProfilePhoto = findViewById(R.id.ivMyProfileProfilePhoto);
         btnEditProfile = findViewById(R.id.btnMyProfileEditeProfile);
@@ -59,34 +67,7 @@ public class Account_Profile_Activity extends AppCompatActivity {
         tvUsername = findViewById(R.id.tvMyProfileUsername);
         btnSignOut = findViewById(R.id.btnMyProfileSingOut);
 
-        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleSignInClient = GoogleSignIn.getClient(Account_Profile_Activity.this, googleSignInOptions);
-
-        GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
-
-        if (googleSignInAccount != null) {
-            String name = googleSignInAccount.getDisplayName();
-            String email = googleSignInAccount.getEmail();
-
-            tvName.setText(name);
-            tvEmailId.setText(email);
-
-            btnSignOut.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            Intent intent = new Intent(Account_Profile_Activity.this, LoginActivity.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    });
-                }
-            });
-        }
     }
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -98,8 +79,49 @@ public class Account_Profile_Activity extends AppCompatActivity {
 
         getMyDetails();
     }
-
     private void getMyDetails() {
+        AsyncHttpClient client = new AsyncHttpClient(); //client Server communication //over network data pass
+        RequestParams params = new RequestParams(); //put the data AsyncHttpClient object
+
+        params.put("username",strUsername);
+
+        client.post(Urls.myDetailsWebService,params,new JsonHttpResponseHandler()
+                {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        super.onSuccess(statusCode, headers, response);
+                        progressDialog.dismiss();
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("getMyDetails");
+
+                            for(int i=0; i<jsonArray.length();i++)
+                            {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                                String strid = jsonObject.getString("id");
+                                String strname = jsonObject.getString("name");
+                                String strmobileno = jsonObject.getString("mobile_no");
+                                String stremailid = jsonObject.getString("email_id");
+                                String strusername = jsonObject.getString("username");
+
+                                tvName.setText(strname);
+                                tvMobileNo.setText(strmobileno);
+                                tvEmailId.setText(stremailid);
+                                tvUsername.setText(strusername);
+                            }
+
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        super.onFailure(statusCode, headers, throwable, errorResponse);
+                        progressDialog.dismiss();
+                        Toast.makeText(Account_Profile_Activity.this, "Server Error", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
 
